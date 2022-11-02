87844ab64f18da5f21d4f4f303e5237689a67cdb
junxian428 <58724748+junxian428@users.noreply.github.com>
2022-11-02 18:46:03 +0800

    Update my_env.sh
    update for hadoop home

--- my_env.sh
+++ my_env.sh
@@ -1,3 +1,9 @@
 #JAVA_HOME
 export JAVA_HOME=/opt/module/jdk1.8.0_341
 export PATH=$PATH:$JAVA_HOME/bin
+
+
+#HADOOP_HOME
+export HADOOP_HOME=/opt/module/hadoop-3.1.3
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
